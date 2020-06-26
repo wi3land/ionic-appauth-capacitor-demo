e193ee2c908b8abed6b7675704833e30b338172e
Malick <diop-malick@hotmail.fr>
2020-06-26 02:01:11 +0200

    Add auth_config in prod environment
    to avoid prod build issue in file replacements

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  auth_config: {
+    identity_client: 'examplemobile',
+    identity_server: 'http://localhost:52652',
+    redirect_url: 'com.appauth.demo://callback',
+    end_session_redirect_url: 'com.appauth.demo://endSession',
+    scopes: 'openid offline_access',
+    usePkce: true
+  }
 };
